4360a1c5ca7f0c247bb0cae47f70c5e9e9d608da
charu sharma <charu7636@gmail.com>
2025-09-01 14:54:35 +0530

    added

---
+++ javascript/var_function/arrowFucntion4.js
@@ -0,0 +1,20 @@
+//sum of all numbers from 1 to 20
+
+
+// function calculation(x){
+//     let s=0;
+//     for(let i=0; i<=x;i++){
+//         s=s+i;
+//     }
+//     return(s);
+// }
+// console.log(calculation(20));
+
+const sum = (x)=>{
+    let s=0;
+   for(let i=0; i<=x;i++){
+        s=s+i;
+    }
+    return(s);
+}
+console.log(sum(20));

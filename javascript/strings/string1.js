ef77d7b789c38ee8bbb34a105be98bf27ef4cdaa
charu sharma <charu7636@gmail.com>
2025-09-01 23:51:00 +0530

    added

---
+++ javascript/strings/string1.js
@@ -0,0 +1,15 @@
+let displayMessage = "I am a student";
+count=0;
+for (let i=0;i<displayMessage.length;i++){
+    if(displayMessage[i]=== "s"){
+   break;}
+   else{
+    console.log(displayMessage[i])
+   count++;
+   }
+   }
+
+    console.log(count);
+    console.log(displayMessage.length)
+
+    
---
+++ javascript/strings/string2.js
@@ -0,0 +1,4 @@
+const newString= new String("I am a student");
+for(let char of newString){
+    console.log(newString);
+}

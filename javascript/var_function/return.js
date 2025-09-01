b17fbbe3dac880252b3368a8a1d43908f59d347a
charu sharma <charu7636@gmail.com>
2025-09-01 12:18:00 +0530

    added

---
+++ javascript/var_function/return.js
@@ -0,0 +1,11 @@
+//return function
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+//calling the function
+let sum = calculateSum(5, 10);
+console.log(`The sum is: ${sum}`);
+
+calculateSum(5, 10);

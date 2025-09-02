e6adeb523ae20bf7c20926313f239b2bfd62ba37
charu sharma <charu7636@gmail.com>
2025-09-02 20:45:49 +0530

    added

---
+++ javascript/strings/string3.js
@@ -0,0 +1,8 @@
+const displayMessage= "I am a student of geeks for geeks";
+const index=3;
+const char = displayMessage.charAt(index);
+console.log(char);
+console.log(displayMessage.length);
+
+const asciicode= displayMessage.charCodeAt(index);
+console.log(asciicode);

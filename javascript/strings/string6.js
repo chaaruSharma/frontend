7a32f49d8f471a5f69ddedbc3b7d056bd72ff51e
charu sharma <charu7636@gmail.com>
2025-09-03 20:28:18 +0530

    added

---
+++ javascript/strings/string6.js
@@ -0,0 +1,18 @@
+const displayMessage ="I love to code in dark modes";
+const vowels ="aeiouAEIOU";
+let flag=false;
+
+for(let char of displayMessage){
+    if(vowels.includes(char))
+    {
+        flag=true;
+        break;
+    }
+    else{
+        flag=false;
+    }
+}
+if(flag===true){
+    console.log("string has vowels");
+}
+else{console.log("string doesn't have vowels");}

17f643e82f9fb9a23b5d819b677e8400127ebe4d
charu sharma <charu7636@gmail.com>
2025-09-04 22:35:01 +0530

    added

---
+++ javascript/array/array8.js
@@ -0,0 +1,9 @@
+//  split and join
+
+const inputstr = "charu sharma";
+const arrofChar = inputstr.split(" ");
+arrofChar.reverse();
+console.log(arrofChar);
+arrofChar.reverse();
+const strOfChar =arrofChar.join("");
+console.log(strOfChar);

07f24b9008d0553204fb7e93d952636d62b77764
charu sharma <charu7636@gmail.com>
2025-09-04 19:53:48 +0530

    added

---
+++ javascript/array/array2.js
@@ -0,0 +1,8 @@
+let arrlength = studentName.length;
+
+for(let i=o ; i<arrlength.length;i++){
+    console.log(arrlength[i]);
+}
+for(let name of arrlength){
+    
+}
---
+++ javascript/array/array3.js
@@ -0,0 +1,12 @@
+fruits=["apple","banana","kiwi","watermelon","cucumber"];
+fruits.push("berries");
+console.log(fruits);
+fruits.push("lichi","strawberry","pineapple");
+console.log(fruits);
+
+let arr1 = [1,3,4];
+let arr2 = [5,6,7];
+arr3=arr1.concat(arr2);
+console.log(arr3);
+arr4=arr1.concat(arr3,arr2);
+console.log(arr4);

af36a43d5284ea3108213dda3159159b966de71c
charu sharma <charu7636@gmail.com>
2025-09-05 15:42:52 +0530

    added

---
+++ javascript/object/object4.js
@@ -0,0 +1,13 @@
+// Property Shorthand
+
+function getObjects(name,city){
+    return{
+        name,city
+    }
+}
+
+const student1= getObjects("charu","Jaipur");
+console.log(student1);
+const student2= getObjects("prakash","mumbai");
+console.log(student2);
+

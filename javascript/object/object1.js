f59580e50e3e80ef447c4ac5c60c6b1655694d70
charu sharma <charu7636@gmail.com>
2025-09-05 07:05:53 +0530

    added

---
+++ javascript/object/object1.js
@@ -0,0 +1,15 @@
+// object(key:property)
+
+// object literal
+
+const personObj={
+    name: "Prakash",
+    age: 99,
+    job: "full stack developer",
+    "is admin": "true"
+};
+console.log(personObj.name);
+console.log(personObj.age);
+console.log(personObj.job);
+
+console.log(personObj["is admin"]);

1b2dd8041afd35d2a3cb65f8132296ae4da254c7
charu sharma <charu7636@gmail.com>
2025-09-09 04:49:29 +0530

    added

---
+++ javascript/DOM/search_Element.js
@@ -0,0 +1,14 @@
+// let StartBtn = document.getElementById("btn-start");
+// console.log(StartBtn);
+
+// let stopbtn= document.getElementById("btn-stop");
+// console.log(stopbtn);
+
+let StartBtn= document.querySelector(".start");
+console.log(StartBtn);
+
+let resetBtn= document.querySelector("#btn-reset");
+console.log(resetBtn);
+
+let button= document.querySelectorAll(".btton");
+console.log(button[2]);
---
+++ javascript/questions/sum_of_even_odd.js
@@ -0,0 +1,6 @@
+// given an array write a function that returns an object with sum of even and odd numbers
+// arr=[1,2,3,4,5,6,7]
+//OUTPUT= {even:12, odd:9}
+
+const arr=[1,2,3,4,5,6,7];
+const result=arr.reduce()

d92785a30724141efcc616bdadd382e5a6a3d16d
charu sharma <charu7636@gmail.com>
2025-09-27 14:05:44 +0530

    add

---
+++ react/first-react-project/src/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+return(
+    <div>
+        <p>this is my first new file</p>
+        <h1>created by me in my first react+ vite project</h1>
+    </div>
+)
+}
+export default Header;

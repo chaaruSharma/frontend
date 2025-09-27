e167ca9acc91c5b8d60ac03b4244256e0a12ec94
charu sharma <charu7636@gmail.com>
2025-09-27 14:06:02 +0530

    add

---
+++ react/first-react-project/src/user.jsx
@@ -0,0 +1,20 @@
+function User() {
+    return(
+        <h1>User Component</h1>
+
+    )
+}
+export default User;
+
+export function Student(){
+    return (
+         <h1>Student Component</h1>
+    );
+}
+export function Teacher(){
+    return (
+         <h1>Teacher Component</h1>
+    );
+}
+
+export const UserKey="12345";

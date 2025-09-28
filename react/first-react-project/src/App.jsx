e6df1e7d84b5e4d8f35d1c3e72f7b94fa912c5fe
charu sharma <charu7636@gmail.com>
2025-09-28 11:49:09 +0530

    add

--- react/first-react-project/src/App.jsx
+++ react/first-react-project/src/App.jsx
@@ -1,18 +1,26 @@
 import Header from "./header";
 import { useState } from "react";
 import User from "./user";
+var name="john doe";
 function Apple(){
   const [count, setCount] = useState(0);
+  const userobj={
+    name:"charu sharma",
+    email:"charu@gmail.com",
+    age: 20
+  }
 return (
   <div>
     <Header />
     <User />
-    <h1>the sum of variable is {sum()}</h1>
+    <p>{userobj.name} email address is {userobj.email} and age is {userobj.age}</p>
+      <h1>the sum of variable is {sum()}</h1>
     <h1>hello World</h1>
     <h2>code step by step</h2>
     <button onClick={() => setCount((count) => count + 1)}>count value : {count}</button>
    <Mango/>
    <Banana/>
+    <Name/>
   </div>
 )
 }
@@ -36,3 +44,17 @@ function sum(){
 // export default Mango;
 // export default Banana;
 export default Apple;
+
+export function Name(){
+  var x=33;
+  var y=67;
+  var sum=x+y;
+
+  return(
+    <div>
+      <div>{name?name: "user not found"}</div>
+<h3>{sum}</h3>
+  <h1>the name of him is {name}and id is {sum}</h1>
+  
+</div>)
+}

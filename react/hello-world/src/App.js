9aca0e12122e926d6eaa0d7b02c8aa799456b745
charu sharma <charu7636@gmail.com>
2025-09-28 11:49:53 +0530

    add

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -5,18 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            hello world        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+            Hello world        </h1>
+       </header>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.  
+        
+       </p>
     </div>
   );
 }

3682ab54c68a9f7595a868d5e46c18766a1e79f4
charu sharma <charu7636@gmail.com>
2026-06-06 04:09:27 +0530

    added

---
+++ javascript/js_Fundamentals/comparison_operator.js
@@ -0,0 +1,10 @@
+
+     
+        // code here
+        const readline= require("readline-sync");
+        var n= readline.question("enter a number");
+        if(n>100){
+            console.log("Big");
+        }
+        console.log("Number");
+    
